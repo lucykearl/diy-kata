9caccb7274fb9fe84343b5f2efd10162650fc0b2
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-06-12 14:44:07 +0100

    Code for the fizzBuzz test

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,17 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+if (number % 3 === 0) {
+    return 'Fizz';
+}
+
+if (number % 5 === 0) {
+    return 'Buzz';
+}
+
+if (number % 5 === 0 || number % 3 === 0) {
+    return 'FizzBuzz';
+}
+
+    return number;
+};
 
 module.exports = fizzBuzz;
